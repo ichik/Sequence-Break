236c1913244f140486d913567c666b23246d10fe
ichik <ichikumer@gmail.com>
2014-08-14 11:20:18 +0400

    Here we go, bye-bye, galleries.

--- functions.php
+++ functions.php
@@ -46,4 +46,9 @@ function customRSS(){
 function customRSSFunc(){
         get_template_part('feed', 'sqncbrk');
 }
+function filter_ptags_on_images($content){
+   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+
+add_filter('the_content', 'filter_ptags_on_images');
 ?>
