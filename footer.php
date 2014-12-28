7638221c8a3702eb7c7be29a45621e12eb2a2266
ichik <ichikumer@gmail.com>
2014-12-28 15:42:44 +0300

    Centering footer properly and cleaning shit in root

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package Stag_Customizer
+ * @subpackage Ink
+ */
+?>
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="copyright">
+				<div class="inside">
+                    <a href="http://ichikumer.ru" class="no-u">ichikumer.ru</a>&nbsp;•&nbsp;<a href="http://twitter.com/ichikumer" class="no-u">@ichikumer</a>&nbsp;•&nbsp;<a href="http://sequencebreak.ru/about" class="no-u">О Sequence Break</a>&nbsp;• <nobr>Подписаться на обновления:&nbsp;&nbsp;<a href="http://feeds.feedburner.com/sequencebreak" class="no-u feed"></a>&nbsp;&nbsp;<a href="http://twitter.com/sqncbrk" class="no-u twitter"></a>&nbsp;&nbsp;<a href="http://facebook.com/sequencebreak"  class="no-u facebook"></a></nobr>
+
+				</div>
+			</div><!-- .copyright -->
+		</footer><!-- #colophon -->
+
+	</div><!-- #content -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
